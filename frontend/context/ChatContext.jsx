36342df441e7857145defb3d76dc0b060188e95a
Patipat003 <28613patipat@gmail.com>
2025-06-28 03:47:49 +0700

    Add ChatContext and enhance ProfilePage with profile editing
    Introduced ChatContext for managing chat state and integrated it into the app. Updated ProfilePage to use AuthContext, allowing users to update their profile picture, name, and bio, with live preview support. Refactored main.jsx to wrap App with ChatProvider.

---
+++ frontend/context/ChatContext.jsx
@@ -0,0 +1,78 @@
+import { createContext, useContext, useState } from "react";
+import { AuthContext } from "./AuthContext";
+
+export const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [unseenMessages, setUnseenMessages] = useState({});
+
+  const { socket, axios } = useContext(AuthContext);
+
+  // Get all users for sidebar
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get("/api/messages/users");
+      if (data.success) {
+        setUsers(data.users);
+        setUnseenMessages(data.unseenMessages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // get message from selected user
+  const getMessages = async (userId) => {
+    try {
+      const { data } = await axios.get(`/api/messages/${userId}`);
+      if (data.success) {
+        setMessages(data.messages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // send message to selected user
+  const sendMessage = async (messageData) => {
+    try {
+      const { data } = await axios.post(
+        `/api/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      if (data.success) {
+        setMessages((prevMessages) => [...prevMessages, data.newMessage]);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // subscribe to messages for selected user
+  const subscribeToMessages = async () => {
+    if (!socket) return;
+
+    socket.on("newMessage", (newMessages) => {
+      if (selectedUser && newMessages.senderId === selectedUser._id) {
+        newMessages.seen = true;
+        setMessages((prevMessages) => [...prevMessages, newMessages]);
+        axios.put(`/api/messages/mark/${newMessages._id}`);
+      } else {
+        setUnseenMessages((prevUnseenMessages) => ({
+          ...prevUnseenMessages,
+          [newMessages.senderId]: prevUnseenMessages[newMessages.senderId]
+            ? prevUnseenMessages[newMessages.senderId] + 1
+            : 1,
+        }));
+      }
+    });
+  };
+
+  const value = {};
+  return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
+};
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,21 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import assets from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const ProfilePage = () => {
+  const { authUser, updateProfile } = useContext(AuthContext);
+
+  const [previewImg, setPreviewImg] = useState(null);
   const [selectedImg, setSelectedImg] = useState(null);
+  const [name, setName] = useState(authUser.fullName);
+  const [bio, setBio] = useState(authUser.bio);
   const navigate = useNavigate();
-  const [name, setName] = useState("Martin Johnson");
-  const [bio, setBio] = useState("Hi everyone, I am using QuickChat");
+
+  useEffect(() => {
+    setPreviewImg(authUser.profilePic);
+  }, [authUser]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    navigate("/");
+    if (!selectedImg) {
+      await updateProfile({
+        fullName: name,
+        bio,
+      });
+      navigate("/");
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(selectedImg);
+    reader.onload = async () => {
+      const base64Image = reader.result;
+      await updateProfile({
+        profilePic: base64Image,
+        fullName: name,
+        bio,
+      });
+      navigate("/");
+    };
   };
 
   return (
     <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
-      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+      <div className="w-5/6 max-w-3xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
         <form
           onSubmit={handleSubmit}
           className="flex flex-col gap-5 p-10 flex-1"
@@ -36,11 +63,11 @@ const ProfilePage = () => {
               src={
                 selectedImg
                   ? URL.createObjectURL(selectedImg)
-                  : assets.avatar_icon
+                  : previewImg || assets.avatar_icon
               }
               alt=""
               className={`w-12 h-12 ${
-                selectedImg && "rounded-full object-cover"
+                (previewImg || selectedImg) && "rounded-full object-cover "
               }`}
             />
             upload profile image
@@ -63,7 +90,7 @@ const ProfilePage = () => {
             rows={4}
           ></textarea>
           <button
-            type="sumbit"
+            type="submit"
             className="bg-gradient-to-r from-purple-400 to-violet-600
             text-white p-2 rounded-full text-lg cursor-pointer"
           >
@@ -71,9 +98,13 @@ const ProfilePage = () => {
           </button>
         </form>
         <img
-          className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10"
-          src={assets.logo_icon}
-          alt=""
+          className="max-w-46 aspect-square mx-16 max-sm:mt-10 rounded-full object-cover"
+          src={
+            selectedImg
+              ? URL.createObjectURL(selectedImg)
+              : previewImg || assets.avatar_icon
+          }
+          alt="profile preview"
         />
       </div>
     </div>
